4e44ddbdd790bc79c1b461ae0264cc19263f785b
amateurManu <oderoemmanuel6@gmail.com>
2024-03-27 18:21:10 +0300

    added file that adds a left child leaf to a node

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a node as left leaf child of another node
+ * @parent: parent node
+ * @value: node value
+ * Return: pointer to new node, NULL if fails
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent ==NULL)
+		return NULL;
+
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->left = parent->left;
+	new->right = NULL;
+
+	if (parent->left != NULL)
+		parent->left->parent = new;
+
+	parent->left = new;
+
+	return (new);
+}
